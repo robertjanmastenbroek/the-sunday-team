fab39682eca9f01f836c1a078d46ea457881613c
Robert-Jan Mastenbroek <motomotosings@gmail.com>
2026-01-12 19:45:02 +0000

    Restore website and update only pricing packages section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import BlogPost1 from './BlogPost1.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BlogPost1 />
+    <App />
   </React.StrictMode>,
 )
